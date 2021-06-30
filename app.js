36cea51f97920c4e6216d867bcde2ef54f29d8a2
Victor Lupascu <sinajeen@gmail.com>
2021-06-30 14:04:58 -0500

    add questions to be used in the prompt

---
+++ app.js
@@ -0,0 +1,178 @@
+const inquirer = require('inquirer');
+
+// Start array of questions 
+const questions = [
+    {
+        type: 'input',
+        message: 'Please provide your github username! (Required)',
+        name: 'github',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please provide your github username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide your email associated to github! (Required)',
+        name: 'email',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please provide your email associated to github!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide a project title! (Required)',
+        name: 'title',
+        validate: tittleInput => {
+            if (tittleInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description about the project! (Required)',
+        name: 'description',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please provide a brief description about the project! (Required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInstall',
+        message: 'Do you want to provide any information on installation?(Important)',
+        default: true
+    },
+    {
+        type: 'input',
+        message: 'Please add any instructions for installation.',
+        name: 'install',
+        when: ({ confirmInstall }) => confirmInstall,
+        validate: instInput => {
+            if (instInput) {
+                return true;
+            } else {
+                console.log('You need to add some instalation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to provide some information on how to use the application?(Important)',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide any information on how to use the application!',
+        when: ({ confirmUsage }) => confirmUsage,
+        validate: useInput => {
+            if (useInput) {
+                return true;
+            } else {
+                console.log('Please provide some information on how to use the application!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Does your project have a license?(Important)',
+        default: true
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'What type of license(s) this project have?',
+        choices: ["MIT", "Apache-2.0", "Creative Commons 1.0", "GPLv3", "WTFPL"],
+        when: ({ confirmLicense }) => confirmLicense,
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('Choose at least one license please!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmBadges',
+        message: 'Do you want to add badges?(Optional)',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'badges',
+        message: 'What badges did you want to add to this project? (Copy them into the input)',
+        when: ({ confirmBadges }) => confirmBadges
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributors',
+        message: 'Are there any contributors to the Project?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Please add the GitHub usernames of the contributors!',
+        when: ({ confirmContributors }) => confirmContributors
+    },
+    {
+        type: 'input',
+        name: 'deployment',
+        message: 'Please provide a link to your deployed application.(Required)',
+        validate: deployInput => {
+            if (deployInput) {
+                return true;
+            } else {
+                console.log('Please provide a link to your deployed application.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'Please provide a link of the screenshot or video-demo of the Project. (Required)',
+        validate: screenshotInput => {
+            if (screenshotInput) {
+                return true;
+            } else {
+                console.log('Please provide a link of the screenshot!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'altscreenshotInput',
+        message: 'Please provide a description of your screenshot. (Optional)'
+    }
+];
+// End array of questions 
+
+
