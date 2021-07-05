5f41b5832aef501398c97927f44afc986f9c3c14
Victor Lupascu <sinajeen@gmail.com>
2021-07-05 13:58:47 -0500

    fix same line badges

--- src/readme-template.js
+++ src/readme-template.js
@@ -31,28 +31,23 @@ const generateLicense = sectionText => {
     }
     else if (sectionText === 'MIT') {
         return `
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-`;
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
     }
     else if (sectionText === 'Apache-2.0') {
         return `
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-`;
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
     }
     else if (sectionText === 'Creative Commons 1.0') {
         return `
-[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
-`;
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)`;
     }
     else if (sectionText === 'GPLv3') {
         return `
-[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-`;
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
     }
     else {
         return `
-[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
-`;
+[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
     }
 };
 
@@ -61,9 +56,7 @@ const generateBadges = sectionText => {
     if (!sectionText) {
         return '';
     };
-    return `
- ![badges](${sectionText}) 
- 
+    return `![badges](${sectionText})
 `;
 };
 
